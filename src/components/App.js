6416de8f7f88fe8f64175b50fb8dcd3c6d411edc
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-10-04 20:28:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,34 @@
-import React,{useState} from "react";
-import './../styles/App.css';
-import Todo form "./Todo.js";
+import React, { useState } from 'react';
+// import './../styles/App.css';
+import Todo from './Todo';
 
 const App = () => {
-  const [list ,setList]=useState([]);
-  const [name,setName]=useState("");
-  function handleSubmit(){
-    let item={
-      id:Math.floor(Math.random()*9000)+1000,
-      name:name,
-    }
-    setList([item,...setList]);
-    setName("");
+  const [list, setList] = useState([]);
+  const [name, setName] = useState('');
+  function handleSubmit() {
+    let item = {
+      id: Math.floor(Math.random() * 9000) + 1000,
+      name: name,
+    };
+    setList([item, ...list]);
+    setName('');
   }
-  function deleteId(id){
-  let newList=list.filter(e=>e.id!==id);
-    setList([newLsit]);
-  };
-  
-  return (
-    <div> 
-    <h1>To-Do List </h1>
-  <form>
-    <input type="text"  onChange={(e)=>setName(e.target.value)} />
-    <button onClick={handleSubmit}>Add ToDo</button>
-    </form>
-  <Todo list={list} deleteId={deleteId}/>  
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  function deleteId(id) {
+    let newList = list.filter(e => e.id !== id);
+    setList(newList);
+  }
 
-export default App
+  return (
+    <div>
+      <h1>To-Do List </h1>
+      <div>
+        <input type='text' onChange={e => setName(e.target.value)} />
+        <button onClick={handleSubmit}>Add ToDo</button>
+      </div>
+      <Todo list={list} deleteId={deleteId} />
+      {/* Do not remove the main div */}
+    </div>
+  );
+};
+
+export default App;
