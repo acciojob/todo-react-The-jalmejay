38dbafdbe28a730f42a151d363931c827a757d9f
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-10-04 18:48:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,13 @@ import './../styles/App.css';
 
 const App = () => {
   return (
-    <div>
+    <div> 
+    <h1>To-Do List </h1>
+  <form>
+    <input type="text" />
+    <button>Add ToDo</button>
+    </form>
+  
         {/* Do not remove the main div */}
     </div>
   )
