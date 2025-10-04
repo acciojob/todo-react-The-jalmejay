05000bc54bb9a867b66055c56b50a8abe51d9b5b
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-10-04 20:36:34 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,11 +3,11 @@ import './../styles/App.css';
 
 const Todo = ({ list, deleteId }) => {
   return (
-    <div>
+    <div className="todoLists">
       {list && list.map((e) => (
-        <div key={e.id} style={{ display: "flex", gap: "10px", marginBottom: "8px" }}>
-          <p style={{ margin: 0 }}>{e.name}</p>
-          <button onClick={() => deleteId(e.id)}>Delete</button>
+        <div key={e.id}  className="list">
+          <p>{e.name}</p>
+          <button class="deleteBtn" onClick={() => deleteId(e.id)}>Delete</button>
         </div>
       ))}
     </div>
