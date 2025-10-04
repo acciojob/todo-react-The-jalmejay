4d0c86e8fd57a77bfd96608962f75697bc4da47e
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-10-04 20:04:59 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,19 @@
 import React from "react";
 
 const Todo (props)=>{
+  const [list,deleteId]=props;
+  
   return(<div>
-    
+    {
+      list&&lsit.map((e)=>{
+    return (
+      <div>
+    <p>{e.name}</p>
+                     <button onClick={deleteId(e.id)}>Delete</button>
+    </div>
+    )
+    });
+    }
     </div>)
 };
 export default Todo;
